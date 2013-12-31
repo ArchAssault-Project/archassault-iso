820de146ae98e634091ba57c9d6e3e054ad997ea
fnord0 <fnord0@riseup.net>
2013-12-30 22:40:47 -0800

    Customized kde ISO for archassault + updated 'root-image/etc/pam.d/passwd' and 'root-image/etc/shadow' in lightweightwm and gnome

--- kde/root-image/root/customize_root_image.sh
+++ kde/root-image/root/customize_root_image.sh
@@ -37,12 +37,31 @@ sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 #start up systemctl processes
 #systemctl enable multi-user.target pacman-init.service choose-mirror.service
 systemctl disable multi-user.target
+systemctl disable sshd
+systemctl disable dhcpcd
 systemctl enable graphical.target pacman-init.service choose-mirror.service
 systemctl enable slim.service
-systemctl enable sshd
 
 #disable network interface names
 ln -s /dev/null /etc/udev/rules.d/80-net-name-slot.rules
+
+#create symlinks to wallpapers
+mkdir -p /usr/share/wallpapers/ArchAssault
+ln -s /usr/share/archassault-artwork/backgrounds/* /usr/share/wallpapers/ArchAssault
+
+#speed-up kde per https://wiki.archlinux.org/index.php/Kde#Speed_up_application_startup
+mkdir -p --mode=755 /home/archassault/.compose-cache/
+chown -R archassault:users /home/archassault/.compose-cache
+
+#set default PDf viewer per https://wiki.archlinux.org/index.php/Kde#Default_PDF_viewer_in_GTK_applications_under_KDE
+sudo -H -n -u archassault -i 'xdg-mime default kde4-okularApplication_pdf.desktop application/pdf'
+
+#set default icons for KDE to oxygen
+ln -sf /usr/share/icons/oxygen /usr/share/icons/default.kde4
+
+#set background for SLiM
+mv /usr/share/slim/themes/default/background.jpg{,.bck}
+ln -s /usr/share/archassault-artwork/backgrounds/archassault-city1.jpg /usr/share/slim/themes/default/background.jpg
 
 # set archassault users password to reset at login
 #chage -d0 archassault
